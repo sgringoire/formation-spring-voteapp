0de4d377b53da333cd222f942e7fc340d6e6ae2a
adminuser <adminuser@adminuser-VirtualBox>
2015-09-13 04:27:02 -0400

    Génération de l'application Angular via Yeoman.

---
+++ voteapp-webapp-ng/src/main/webapp/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name voteNgApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the voteNgApp
+ */
+angular.module('voteNgApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ voteapp-webapp-ng/src/main/webapp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name voteNgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the voteNgApp
+ */
+angular.module('voteNgApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
